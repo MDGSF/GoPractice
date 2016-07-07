9502ba767c1415c4dffa97c15964ca7c881010a9
mdgsf <1342042894@qq.com>
2016-07-07 09:40:55 +0800

    go install

--- theWayToGo/9_5_pack/src/test/main.go
+++ theWayToGo/9_5_pack/src/test/main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-	"./pack1/pack1"
 	"fmt"
+	"pack1"
 )
 
 func main() {
 	var test1 string
 	test1 = pack1.ReturnStr()
 	fmt.Printf("ReturnStr from package1: %s\n", test1)
+	fmt.Printf("main() hello\n")
 }
