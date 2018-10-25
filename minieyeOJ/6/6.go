22bc13a6cbff4c2f07bbdc468d73544b223740a5
huangjian <1342042894@qq.com>
2018-10-25 19:31:44 +0800

    add minieyeOJ 6

---
+++ minieyeOJ/6/6.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	root := NewTrie()
+
+	var n int
+	fmt.Scanf("%d", &n)
+
+	words := make([]string, 0)
+	for i := 0; i < n; i++ {
+		var word string
+		fmt.Scanf("%s", &word)
+		words = append(words, word)
+		root.Insert(word)
+	}
+
+	for _, v := range words {
+		fmt.Printf("%v %v\n", v, root.FindPrefix(v))
+	}
+}
+
+type Trie struct {
+	C      byte
+	IsWord bool
+	Word   string
+	Next   map[byte]*Trie
+	Parent *Trie
+}
+
+func NewTrie() *Trie {
+	t := &Trie{}
+	t.Next = make(map[byte]*Trie)
+	return t
+}
+
+func (t *Trie) Insert(word string) {
+	insert(t, word, word)
+}
+
+func insert(t *Trie, word, nochangeWord string) {
+	if len(word) == 0 {
+		t.IsWord = true
+		t.Word = nochangeWord
+		return
+	}
+
+	c := byte(word[0])
+	n, ok := t.Next[c]
+	if !ok {
+		newNode := NewTrie()
+		newNode.C = c
+		newNode.Parent = t
+		t.Next[c] = newNode
+		n = newNode
+	}
+	insert(n, word[1:], nochangeWord)
+}
+
+func (t *Trie) Index(word string) *Trie {
+	return indexWord(t, word)
+}
+
+func indexWord(t *Trie, word string) *Trie {
+	if len(word) == 0 {
+		return t
+	}
+
+	c := byte(word[0])
+	n := t.Next[c]
+	return indexWord(n, word[1:])
+}
+
+func (t *Trie) FindPrefix(word string) string {
+	if len(word) == 1 {
+		return word
+	}
+
+	node := t.Index(word)
+	if len(node.Next) > 0 {
+		return word
+	}
+
+	p1 := node.Parent
+	p2 := node
+
+	for p1 != nil && p1.Parent != nil {
+		if p1.IsWord {
+			return p1.Word + string(p2.C)
+		}
+
+		if len(p1.Next) > 1 {
+			return getWord(p1) + string(p2.C)
+		}
+
+		p1 = p1.Parent
+		p2 = p2.Parent
+	}
+
+	return string(p2.C)
+}
+
+func getWord(t *Trie) string {
+	word := innerGetWord(t, "")
+	wordByte := []byte(word)
+	newByte := make([]byte, 0)
+	i := len(wordByte) - 1
+	for i >= 0 {
+		newByte = append(newByte, wordByte[i])
+		i--
+	}
+	return string(newByte)
+}
+
+func innerGetWord(t *Trie, s string) string {
+	if t == nil {
+		return s
+	}
+
+	return innerGetWord(t.Parent, s+string(t.C))
+}
