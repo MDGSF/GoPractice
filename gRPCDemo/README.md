f4489b97ebdd4e5b197edd711bed51135ee6d61a
huangjian <1342042894@qq.com>
2019-01-16 11:42:31 +0800

    add gRPC demo

---
+++ gRPCDemo/README.md
@@ -0,0 +1,9 @@
+
+https://github.com/grpc/grpc
+https://github.com/grpc/grpc-go
+https://grpc.io/docs/guides/
+
+```
+protoc --go_out=plugins=grpc:. helloworld.proto
+```
+
---
+++ gRPCDemo/greeter_client/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	pb "github.com/MDGSF/GoPractice/gRPCDemo/helloworld"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	c := pb.NewGreeterClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: "Huangjian"})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.Message)
+
+	r, err = c.SayHelloAgain(ctx, &pb.HelloRequest{Name: "Huangjian"})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.Message)
+}
